d10a8708640df0a12e9a74a2e197b00ceacdf8f4
ateeq72 <ahmedateeq64@gmail.com>
2015-04-21 09:51:50 +0000

    Messing with UI

--- home_auto_main.py
+++ home_auto_main.py
@@ -11,7 +11,11 @@ import subprocess
 import mysql
 import smtplib
 import MySQLdb
+import pymysql
+import web
+
 from DBUtils.PersistentDB import PersistentDB
+
 
 
 GPIO.setmode(GPIO.BOARD)
@@ -28,9 +32,13 @@ GPIO.setup(re1,GPIO.OUT)
 GPIO.setup(re2,GPIO.OUT)
 GPIO.setup(re3,GPIO.OUT)
 
+def connect(thread_index): 
+        cherrypy.thread_data.db = MySQLdb.connect('localhost', 'ateeq', 'khader6@', 'usage_homeauto') 
+    
+cherrypy.engine.subscribe('start_thread', connect)
 
 class AteeqHomeAutomation:
-    """ Sample request handler class. """
+
     @cherrypy.expose
     def index(self):
         my_ip = ([(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
@@ -38,9 +46,9 @@ class AteeqHomeAutomation:
 <html>
     <head>
         <meta charset="utf-8" />
-        //<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0;" />
-        //<meta name="viewport" content="width=device-width"/>
-        //<meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=1;" />
+        <meta name="viewport" content="width=device-width"/>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <title>
          Welcome To Ateeq's Home Automation
         </title>
@@ -49,6 +57,8 @@ class AteeqHomeAutomation:
         </script>
         <script src="static/jquery.mobile-1.0.1.min.js">
         </script>
+
+
         <script type="text/javascript">
         $(document).ready(function() {
             //stop the page from doing a stretch from the top when dragged ;
@@ -61,17 +71,23 @@ class AteeqHomeAutomation:
             $("#power2").change(function () {$.post('/request',{key_pressed:"power2_"+$(this).val()})});
             $("#power3").change(function () {$.post('/request',{key_pressed:"power3_"+$(this).val()})});
             $("#stream").change(function () {$.post('/request',{key_pressed:"stream_"+$(this).val()})});
+	    $("#empty").click(function () {$.post('/request',{key_pressed:"empty"})});
         });
         </script>
        </head>
     <body style="overflow: hidden;overflow-x:hidden;" onload="createImageLayer();">
         <div data-role="page" data-theme="a" id="page1">
             <div data-theme="a" data-role="header" data-position="">
+
+
                 <h5>
                     Ateeq's <br> Home Automation
                 </h5>
+		
             </div>
-            <div data-role="content">
+
+
+           <div data-role="content">
                 <div data-role="fieldcontain">
                     <fieldset data-role="controlgroup">
                         <label for="auto">
@@ -119,7 +135,7 @@ class AteeqHomeAutomation:
                             </option>
                         </select><br>
                        <label for="stream">
-                       Use Me to toggle live<a href="http://%s:8091/stream_webcam.html"> webcam stream! </a> 
+      Stream : <a href="http://%s:8091/stream_webcam.html"> local! </a> & <a href="http://ateeqhomeautomationstream.ngrok.com/stream_webcam.html">Worldwide Stream!</a>
                         </label>
                         <select name="stream" id="stream" data-theme="a" data-role="slider">
                             <option value="off">
@@ -128,22 +144,40 @@ class AteeqHomeAutomation:
                             <option value="on">
                                 On
                             </option>
-                        </select><br>
-     <center>
-            <form method="post" action="http://%s:8090/processform"> 
-            Email: <input type="email" name="email"><br> 
-            <input type="submit" value="Submit"> </form><br>
-             <a href="/static/qrcode.html">QR-Code</a> to get connected to This WiFi!<br>
-             Check Switches <a href="http://%s/status.html">history</a><br>
-     </center>
-
-                    </fieldset>
-                </div>
-            </div>
+                        </select>
+</fieldset>
+<div data-role="content">
+  <div class="ui-grid-a">
+         <div class="ui-block-a">                 
+            <form method="post" action="/processform"> 
+            <input type="email" value="E-mail" name="email"> 
+         </div>
+         <div class="ui-block-b">
+             <input type="submit" value="Submit"> </form>
+         </div>
+  </div>
+  <div class="ui-grid-a">
+         
+	    <a href="/qrcode">QR-Code</a> to get connected to This WiFi!<br>
+         
+  </div>
+  <div class="ui-grid-a"><center>----</center>
+  </div>
+  <div class="ui-grid-a">
+         <div class="ui-block-a"> 
+             Check Switches <a href="http://%s/status.html">history</a>
+         </div>
+         <div class="ui-block-b"> 
+	     <input type="button" id="empty" data-role="button" data-transition="fade"  value="Clear History!"/>
+         </div>
+ 
+  </div>
 </div>
-     </body>
+</div>
+</div>
+</body>
 </html>
-''' % (my_ip,my_ip,my_ip)
+''' % (my_ip,my_ip)
     @cherrypy.expose
     def request(self, **data):
         # Then to access the data do the following
@@ -183,21 +217,51 @@ class AteeqHomeAutomation:
             mysql.insertReading('switch_three','off')
         elif key == "stream_on":
             print "Stream On!"
-            os.system("sudo service mjpg-streamer start")
+            subprocess.call("sudo service mjpg-streamer start", shell=True)
         elif key == "stream_off":
             print "Stream Off!"
             os.system("sudo service mjpg-streamer stop")
-
-
+	elif key == "empty":
+            print "Emptying Table!"
+            mysql.removeEvery()
+	  
         else:
             print key
 
+
     @cherrypy.expose
+    def qrcode(self):
+        return '''<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="static/jquery.mobile-1.0.1.min.css" />
+        <script src="static/jquery.min.js">
+        </script>
+        <script src="static/jquery.mobile-1.0.1.min.js">
+        </script>
+    </head>
+    <body style="overflow: hidden;overflow-x:hidden;" onload="createImageLayer();">
+        <div data-role="page" data-theme="a" id="page1">
+            <div data-theme="a" data-role="header" data-position="">
+                <h5>
+                    Ateeq's <br> Home Automation
+                </h5>
+            </div>
+    <center>
+    <img src="/static/wifi.png" /><br>
+    Scan this code with your Smart Phone usind Qr-Code scanner!
+    </center>
+    </body>
+</html>'''
+        
+
+    cherrypy.expose
     def processform(self, email):
         smtpserver = 'smtp.gmail.com:587'
         authreq = 1
         smtpuser='ahmedateeq64@gmail.com'
-        smtppass='<secret>'
+        smtppass='flmjlspxjmdltemeat'
         FROM = 'ahmedateeq64@gmail.com'
         TO = [email]
         SUBJECT = "Hi Greetings from Ateeq."
@@ -223,8 +287,20 @@ class AteeqHomeAutomation:
 <html>
     <head>
         <meta charset="utf-8" />
+        <link rel="stylesheet" href="static/jquery.mobile-1.0.1.min.css" />
+        <script src="static/jquery.min.js">
+        </script>
+        <script src="static/jquery.mobile-1.0.1.min.js">
+        </script>
     </head>
-    <body>
+    <body style="overflow: hidden;overflow-x:hidden;" onload="createImageLayer();">
+        <div data-role="page" data-theme="a" id="page1">
+            <div data-theme="a" data-role="header" data-position="">
+                <h5>
+                    Ateeq's <br> Home Automation
+                </h5>
+
+            </div>
     <center>
     <h1>Mail sent to %s </h1>
     </center>
